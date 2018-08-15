4f1bba312fa2af162080d3ebf417773cd7a5a40b
Zach Biri <zachbiri@gmail.com>
2018-08-14 21:22:47 -0500

    Styling...

--- src/components/Nav/NavIcon/NavIcon.js
+++ src/components/Nav/NavIcon/NavIcon.js
@@ -3,13 +3,13 @@ import "./NavIcon.css";
 const navIcon = props => {
   let faIcon = null;
   if (props.type === "home") {
-    faIcon = <i title="Home" class="fas fa-user-circle" />;
+    faIcon = <i title="Home" className="fas fa-user-circle" />;
   }
   if (props.type === "web") {
-    faIcon = <i title="Web development" class="fas fa-code" />;
+    faIcon = <i title="Web development" className="fas fa-code" />;
   }
   if (props.type === "music") {
-    faIcon = <i title="Music and audio" class="fas fa-music" />;
+    faIcon = <i title="Music and audio" className="fas fa-music" />;
   }
 
   return (
