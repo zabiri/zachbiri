3abb2965d2bbf72715d1c16f481469b6fdb3769c
Zach Biri <zachbiri@gmail.com>
2018-08-17 15:57:08 -0500

    Setup of other sections

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div class="card">
+      <img src="img_avatar.png" alt="Avatar" style="width:100%" />
+      <div class="container">
+        <h4>
+          <b>John Doe</b>
+        </h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+  );
+};
+
+export default card;
