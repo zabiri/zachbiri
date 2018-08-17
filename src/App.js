2e58ca880bed03a7d834a8b5af7ac4aeca76b724
Zach Biri <zachbiri@gmail.com>
2018-08-17 16:13:41 -0500

    Setting up routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
-import Border from './components/Border/Border'
-import Content from './containers/Content/Content'
-import Nav from './components/Nav/Nav'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Border from "./components/Border/Border";
+import Content from "./containers/Content/Content";
+import Nav from "./components/Nav/Nav";
+import Intro from "./pages/Intro/Intro";
+import Head from './components/Art/Head/Head'
+import RE20 from './components/Art/RE20/RE20'
+
 import "./App.css";
 
 class App extends Component {
+  state = {
+    home: {
+      left: <Intro/>,
+      right: <Head/>
+    },
+    audio: {
+      left: <Intro/>,
+      right: <RE20/>
+    },
+    web: {
+      left: <Intro/>,
+      right: <RE20/>
+    }
+  }
   render() {
     return (
-      <main className="App">
-        <Border/>
-        <Nav/>
-        <Content/>
-      </main>
+      <BrowserRouter>
+        <main className="App">
+          <Border />
+          <Nav />
+          <Switch>
+            <Route
+              path="/web"
+              render={() => <Content left={this.state.web.left} right={this.state.web.right} />}
+            />
+            <Route
+              path="/audio"
+              render={() => <Content left={this.state.audio.left} right={this.state.audio.right} />}
+            />
+            <Route
+              path="/"
+              render={() => <Content left={this.state.home.left} right={this.state.home.right} />}
+            />
+          </Switch>
+        </main>
+      </BrowserRouter>
     );
   }
 }
--- src/containers/Content/Content.js
+++ src/containers/Content/Content.js
@@ -1,20 +1,12 @@
-import React, { Component } from "react";
-import Intro from "../../pages/Intro/Intro";
-import Head from '../../components/Art/Head/Head'
-import RE20 from '../../components/Art/RE20/RE20'
-
-import "./Content.css";
-export class Content extends Component {
-  render() {
-    let leftPane = <Intro />;
-    let rightPane = <RE20 />;
-    return (
-      <main className="Content">
-        {leftPane}
-        {rightPane}
+import React from 'react'
+import './Content.css'
+const content = (props) => {
+  return (
+    <main className="Content">
+        {props.left}
+        {props.right}
       </main>
-    );
-  }
+  )
 }
 
-export default Content;
+export default content
