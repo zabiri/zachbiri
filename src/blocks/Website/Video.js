d7767b4241446e8128e87add330b7504a96eb3d1
Zach Biri <zachbiri@gmail.com>
2018-08-26 22:24:03 -0500

    trying to fix video headers

--- src/blocks/Website/Video.js
+++ src/blocks/Website/Video.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 const Video = styled.video.attrs({
-  loop: "true",
-  autoPlay: "true"
+  muted: true,
+  playsInline: true,
+  autoPlay: true,
+  loop: true,
 })`
   width: 100%;
   border-radius: .5rem .5rem 0 0
